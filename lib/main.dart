bd0897ab1b14bcf50db28902f6734d104d8cdd53
codewithumar <codewithumar@gmail.com>
2022-07-20 12:49:27 +0500

    Phase 2 , Almost done with ui and doing Database

--- lib/main.dart
+++ lib/main.dart
@@ -37,10 +37,8 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Column(
-        // ignore: prefer_const_literals_to_create_immutables
         children: [TodoCard(), const SizedBox(height: 5), UserInput()],
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
